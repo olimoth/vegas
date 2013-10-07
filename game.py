cd629d34bba1d3a7b5bcadb1f52ba8638716b863
olimoth <olimoth@gmail.com>
2013-10-07 11:58:20 +0000

    Added cash card values to rules

--- game.py
+++ game.py
@@ -3,6 +3,16 @@ The Las Vegas board game consists of:
     
     * Six casinos, one for each dice value
     * Eight six-sided dice per player
+    * Cash cards:
+        $60,000 (5)
+        $70,000 (5)
+        $80,000 (5)
+        $90,000 (5)
+        $10,000 (6)
+        $40,000 (6)
+        $50,000 (6)
+        $20,000 (8)
+        $30,000 (8)
     * Between 2 and 5 players
 
 Setup:
@@ -19,6 +29,8 @@ For each player:
 End of round:
     For each card on the casino in decending order of value:
         Award to player with largest number of dice, if no other player has the same number of dice
+    Shuffle the cash cards
+    Repeat setup, move starting card to next player
 
 Winner is the player with the most money after 4 rounds
 '''
